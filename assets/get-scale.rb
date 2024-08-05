ba0e364f84d52b42c05798051859dd3f905e4eec
Yiling-Huo <huo.yiling.96@gmail.com>
2024-08-05 15:10:19 +0100

    update

--- assets/get-scale.rb
+++ assets/get-scale.rb
@@ -4,17 +4,17 @@ require 'csv'
 # Create or open a CSV file for writing
 CSV.open("countries_scales.csv", "w") do |csv|
   # Write the header row
-  csv << ["Country Code", "Min lng", "Max Lng", "Min Lat", "Max Lat", "axis", "value"]
+  csv << ["Country Code", "Min lng", "Max lng", "Min lat", "Max lat", "axis", "value"]
 
   # Iterate through all countries
   ISO3166::Country.all.each do |country|
     # Get the country code
     country_code = country.alpha2
 
-    min_lo = country.min_longitude # => '45'
-    min_la = country.min_latitude # => '22.166667'
-    max_lo = country.max_longitude # => '58'
-    max_la = country.max_latitude # => '26.133333'
+    min_lo = country.min_longitude
+    min_la = country.min_latitude
+    max_lo = country.max_longitude
+    max_la = country.max_latitude
 
     # average latitude to calculate lo difference (yue)
     avg_la = country.latitude
@@ -33,16 +33,13 @@ CSV.open("countries_scales.csv", "w") do |csv|
     axis = "lat"
     value = la_diff_km
 
-    # latitide max 90, but longitude max 180, so need multiply by 2
+    # decide whether to use longitude or latitude as scale (decide whether country is wider or longer)
     if lo_diff_km > la_diff_km
         axis = "lng"
         value = lo_diff_km
     end
 
-    # Get the surface area (if available)
-    # surface_area = country.area
-
-    # Write the country code and surface area to the CSV file
+    # Write the country code and info to the CSV file
     csv << [country_code, min_lo, max_lo, min_la, max_la, axis, value]
   end
 end
